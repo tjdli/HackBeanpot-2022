a697fd8715e083501cc207ede364601cb30f576a
tjdli <tristen.jd.li@gmail.com>
2022-02-12 01:13:06 -0500

    Added new words to the js

---
+++ star-gazing/src/About.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function About() {
+    return (
+      <div className="About">
+        <header className="About-header">
+          <p>Random Bullshit</p>
+        </header>
+      </div>
+    );
+  };
+  
+  export default About;
