108f0ee44899b8ff6e054637a4e660c73d7ec557
tjdli <tristen.jd.li@gmail.com>
2022-02-12 18:11:07 -0500

    Added location form

--- star_gazing/star_site/urls.py
+++ star_gazing/star_site/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'star_site'
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
-    # path('star_locations', views.StarLocationsView.as_view(), name='star_locations'),
-    path('star_locations', views.location_list, name='location_list'),
+    path('star_locations', views.location_list, name='star_locations'),
+    path('add_location', views.location_form, name='add_location'),
+
 ]
