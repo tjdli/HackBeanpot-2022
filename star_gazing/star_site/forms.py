8d301c599a6440c796e33f4ba543d8be15325303
tjdli <tristen.jd.li@gmail.com>
2022-02-12 20:13:41 -0500

    Added sorted file

--- star_gazing/star_site/forms.py
+++ star_gazing/star_site/forms.py
@@ -7,3 +7,7 @@ class LocationForm(forms.ModelForm):
     class Meta:
         model = Location
         fields = ['name', 'position', 'description']
+
+
+class AddressForm(forms.Form):
+    address = forms.CharField(max_length=255)
--- star_gazing/star_site/views.py
+++ star_gazing/star_site/views.py
@@ -5,8 +5,10 @@ from django.shortcuts import render
 from django.utils import timezone
 from django.views import generic
 
-from .forms import LocationForm
+from .forms import LocationForm, AddressForm
 from .models import Location
+
+import requests, json
 
 
 class HomePageView(generic.TemplateView):
@@ -25,8 +27,16 @@ class StarLocationsView(generic.TemplateView):
 
 
 def location_list(request):
-    locations = Location.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')
-    return render(request, "star_site/star_locations.html", {'locations': locations})
+    form = AddressForm()
+    ordered_locations = Location.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')
+    locations = Location.objects.filter(pub_date__lte=timezone.now()).all()
+    if request.method == "POST":
+        form = AddressForm(request.POST)
+        if form.is_valid():
+
+            sorted_locations = sorted(locations, key=lambda position: sort_by_distance(form.cleaned_data["address"], position.position))
+            return render(request, "star_site/star_locations.html", {'form': form, 'locations': sorted_locations})
+    return render(request, "star_site/star_locations.html", {'form': form, 'locations': ordered_locations})
 
 
 def location_form(request):
@@ -38,3 +48,25 @@ def location_form(request):
             return HttpResponseRedirect('/star_locations')
     # locations = Location.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')
     return render(request, "star_site/location_add_form.html", {'form': form})
+
+
+def distance_list(request):
+    form = AddressForm()
+    ordered_locations = Location.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')
+    locations = Location.objects.filter(pub_date__lte=timezone.now()).all()
+    if request.method == "POST":
+        form = AddressForm(request.POST)
+        if form.is_valid():
+            sorted_locations = sorted(locations, key=lambda position: sort_by_distance(form.address, position))
+            return render(request, "star_site/star_locations.html", {'locations': sorted_locations})
+    return render(request, "star_site/star_locations.html", {'locations': ordered_locations})
+
+
+def sort_by_distance(source, dest):
+    url = 'https://maps.googleapis.com/maps/api/distancematrix/json?'
+    r = requests.get(url + '&destinations = ' + dest +
+                     'origins = ' + source +
+                     '&key = ' + "AIzaSyDV6nqMJ7_iY1nU3reiUDrltej_Laf5BCw")
+    result = r.json()
+    print(result)
+    return result['rows'][0]['elements'][0]["distance"]["value"]
